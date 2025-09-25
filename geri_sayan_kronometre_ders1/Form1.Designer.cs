efaa95ede1c6389f707679a7c3f98930dbd3ddd5
Türker Efe Şahin <turkerefe11@gmail.com>
2025-09-25 14:38:37 +0300

    Add files via upload

---
+++ geri_sayan_kronometre_ders1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace gorselprog2ders1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SAAT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DK";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "SS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "SAAT";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(143, 121);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "DK";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "SN";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(273, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "SS";
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(345, 55);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(96, 79);
+            this.btnBasla.TabIndex = 8;
+            this.btnBasla.Text = "BASLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 219);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ geri_sayan_kronometre_ders1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gorselprog2ders1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool esitlendi = false;
+        bool gerisayim = false;
+
+        int salise = 0;
+        int saniye = DateTime.Now.Second - 5;
+        int dakika = DateTime.Now.Minute;
+        int saat = DateTime.Now.Hour;
+
+
+        int sistemsaat = DateTime.Now.Hour;
+        int sistemdakika = DateTime.Now.Minute;
+        int sistemsaniye = DateTime.Now.Second;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (esitlendi == false && gerisayim == false)
+            {
+                label1.Text = saat.ToString();
+                label2.Text = dakika.ToString();
+                label3.Text = saniye.ToString();
+                label4.Text = salise.ToString();
+                timer1.Interval = 10;
+                timer1.Start();
+                btnBasla.Enabled = false;
+            }
+            if (esitlendi == true && gerisayim == false)
+            {
+                gerisayim = true;
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (esitlendi == true && gerisayim == false)
+            {
+                btnBasla.BackColor = Color.Green;
+                salise++;
+                if (salise == 100)
+                {
+                    saniye++;
+                    salise = 0;
+                }
+
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                }
+
+                if (dakika == 60)
+                {
+                    saat++;
+                    dakika = 0;
+                }
+                label1.Text = saat.ToString();
+                label2.Text = dakika.ToString();
+                label3.Text = saniye.ToString();
+                label4.Text = salise.ToString();
+            }
+            if (esitlendi == false && gerisayim == false)
+            {
+                salise++;
+                if (salise == 100)
+                {
+                    saniye++;
+                    salise = 0;
+                }
+
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                }
+
+                if (dakika == 60)
+                {
+                    saat++;
+                    dakika = 0;
+                }
+                label1.Text = saat.ToString();
+                label2.Text = dakika.ToString();
+                label3.Text = saniye.ToString();
+                label4.Text = salise.ToString();
+            }
+
+            if (esitlendi == true && gerisayim == true)
+            {
+                btnBasla.Enabled = true;
+                btnBasla.BackColor = Color.Green;
+
+                label5.Text = saat.ToString();
+                label6.Text = dakika.ToString();
+                label7.Text = saniye.ToString();
+                label8.Text = salise.ToString();
+
+                if (salise == 0)
+                {
+                    salise = 100;
+                    saniye--;
+                }
+
+                salise--;
+
+                if (saniye == 0)
+                {
+                    dakika--;
+                    saniye = 60;
+                }
+
+                if (dakika == 00)
+                {
+                    saat--;
+                    dakika = 60;
+                }
+            }
+            if (saat == sistemsaat && dakika == sistemdakika && saniye == sistemsaniye && esitlendi == false)
+            {
+                esitlendi = true;
+                btnBasla.Enabled = true;
+            }
+
+        }
+
+    }
+}
